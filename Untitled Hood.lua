adebc1b30dc6ccf782c79d1d11341356a5b40c6d
xlostpexz <98389349+xlostpexz@users.noreply.github.com>
2022-04-10 18:48:19 +0700

    Update Untitled Hood.lua

--- Untitled Hood.lua
+++ Untitled Hood.lua
@@ -4,6 +4,10 @@ local venyx = library.new("Winnable Hub | Untitled Hood | Give Money", 501310957
  
 local page = venyx:addPage("Give Money", 5012544693)
 local farm = page:addSection("You Need to Have Revolver")
+local page = venyx:addPage("Teleport", 5012544693)
+local tp = page:addSection("Ez Teleport")
+local page = venyx:addPage("Misc", 5012544693)
+local misc = page:addSection("Other")
 local theme = venyx:addPage("Theme", 5012544693)
 local colors = theme:addSection("Colors")
 
@@ -13,16 +17,83 @@ for i,v in pairs(game:GetService("Players"):GetChildren()) do
    table.insert(mons,v.Name)
 end
 
-farm:addButton("Food Store ร้านอาหาร", function()
+misc:addButton("Toxic Music 18+", function()
+
+local args = {
+    [1] = "Boombox",
+    [2] = "6963097644"
+}
+
+game:GetService("ReplicatedStorage"):FindFirstChild(".gg/untitledhood"):FireServer(unpack(args))
+    
+end)
+
+tp:addButton("Helicopter For AFK", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(120.76371, 128.117004, -1280.0918, -0.772315145, 2.55131916e-08, 0.635239542, 9.35894118e-09, 1, -2.87846369e-08, -0.635239542, -1.62856431e-08, -0.772315145)
+end)
+
+tp:addButton("High Armor เกราะใหญ่", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(240.015671, 27.6787167, -1047.90271, -0.760237992, -7.32069623e-08, 0.649644613, -2.50909178e-08, 1, 8.33253964e-08, -0.649644613, 4.7046953e-08, -0.760237992)
+end)
+
+tp:addButton("Food Store ร้านอาหาร", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-491.758972, 9.04687405, -1151.94543, -0.254918456, -5.60639428e-08, -0.966962576, 3.29896608e-08, 1, -6.6676435e-08, 0.966962576, -4.88968226e-08, -0.254918456)
 end)
 
-farm:addButton("Double Barrel Buy ลุกซองคู่", function()
+tp:addButton("Double Barrel Buy ลุกซองคู่", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-555.402039, 40.1311607, -1721.48181, 1, 0, 0, 0, 1, 0, 0, 0, 1)    
 end)
 
-farm:addButton("Revolver Buy ลูกโม่", function()
+tp:addButton("Revolver Buy ลูกโม่", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-313.566406, 19.6537323, -1178.69446, -0.999712467, 0, 0.0239957981, 0, 1, 0, -0.0239957981, 0, -0.999712467)
+end)
+
+farm:addButton("Give Money 1M", function()
+local player = Select
+
+game.ReplicatedStorage:FindFirstChild(".gg/untitledhood"):FireServer(
+    "Reload",
+    {
+        Name = "[Revolver]", 
+        Ammo = {Value = math.huge*9e9},
+        MaxAmmo = {Value = 0}
+    }
+)
+task.wait(2)
+
+
+game.ReplicatedStorage:FindFirstChild(".gg/untitledhood"):FireServer(
+    "Reload",
+    {
+        Name = "[Revolver]",
+        Ammo = game:GetService("Players")[player].DataFolder.Currency,
+        MaxAmmo = {Value = 1000000} 
+    }
+)
+end)
+
+farm:addButton("Give Money 100K", function()
+local player = Select
+
+game.ReplicatedStorage:FindFirstChild(".gg/untitledhood"):FireServer(
+    "Reload",
+    {
+        Name = "[Revolver]", 
+        Ammo = {Value = math.huge*9e9},
+        MaxAmmo = {Value = 0}
+    }
+)
+task.wait(2)
+
+
+game.ReplicatedStorage:FindFirstChild(".gg/untitledhood"):FireServer(
+    "Reload",
+    {
+        Name = "[Revolver]",
+        Ammo = game:GetService("Players")[player].DataFolder.Currency,
+        MaxAmmo = {Value = 100000} 
+    }
+)
 end)
 
 farm:addButton("Give Money Inf", function()
@@ -44,7 +115,7 @@ game.ReplicatedStorage:FindFirstChild(".gg/untitledhood"):FireServer(
     {
         Name = "[Revolver]",
         Ammo = game:GetService("Players")[player].DataFolder.Currency,
-        MaxAmmo = {Value = 9999999999999 * 9} 
+        MaxAmmo = {Value = 99999999999999} 
     }
 )
 end)
@@ -106,6 +177,10 @@ farm:addButton("Refresh", function()
 for i,v in pairs(game:GetService("Players"):GetChildren()) do
    table.insert(mons,v.Name)
 end
+end)
+
+farm:addButton("Teleport To Players", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[Select].Character.HumanoidRootPart.CFrame * CFrame.new(0,0,0)
 end)
 
 farm:addToggle("ESP", "", function(value)
